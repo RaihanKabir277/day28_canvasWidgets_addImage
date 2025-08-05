447d638a57eb3a3e86eee6eab3fb8ac4a798fa99
RaihanKabir277 <raihank0198@gmail.com>
2025-08-05 18:00:38 +0600

    GUI added successfully for promodoro timing here

--- main.py
+++ main.py
@@ -16,15 +16,29 @@ LONG_BREAK_MIN = 20
 window = Tk()
 window.title("Pomodoro technique")
 window.minsize(500, 300)
-window.config(padx=100, pady=50, bg=YELLOW)
+window.config(padx=200, pady=50, bg=YELLOW)
+
+timer_label = Label(text="Timer", fg=GREEN,bg=YELLOW, font=(FONT_NAME, 50, "bold", ))
+timer_label.grid(column=1, row=0)
+
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)   #here x = 100 and y = 112 
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 
-canvas.pack()
+canvas.grid(column=1, row=1)
 
+start_btn = Button(text="Start", highlightthickness=0)
+start_btn.grid(column=0, row=2)
+start_btn.config(padx=5,pady=5)
+
+reset_btn = Button(text="Reset",highlightthickness=0)
+reset_btn.grid(column=2 , row=2)
+reset_btn.config(padx=5, pady=5)
+
+check_mark = Label(text="✓",font=(FONT_NAME,25,"bold"), fg=RED, bg=YELLOW, highlightthickness=0, pady=20)
+check_mark.grid(column=1, row=2)
 
 window.mainloop()
 
